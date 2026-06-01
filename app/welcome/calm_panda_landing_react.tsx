75101a7a64739a5d71d4f29a3d99d5b0b5d3f82a
AndreiXaker <ibragim.asnaev@yandex.ru>
2026-06-01 21:34:29 +0300

    test commit_2

--- app/welcome/calm_panda_landing_react.tsx
+++ app/welcome/calm_panda_landing_react.tsx
@@ -227,7 +227,6 @@ export default function CalmPandaLanding() {
               </div>
             </div>
           </div>
-
           <div className="relative">
             <div className="rounded-[2rem] bg-white/80 p-4 shadow-2xl ring-1 ring-sky-100 backdrop-blur">
               <img
